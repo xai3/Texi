462e936e49e30c9d2d72ccb3dc30c84e854b5fad
yukiasai <yukiasai@gmail.com>
2015-12-06 17:39:55 +0900

    Initial Commit

---
+++ Texi/Texi.h
@@ -0,0 +1,19 @@
+//
+//  Texi.h
+//  Texi
+//
+//  Created by yuki on 2015/12/06.
+//  Copyright © 2015年 yukia.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Texi.
+FOUNDATION_EXPORT double TexiVersionNumber;
+
+//! Project version string for Texi.
+FOUNDATION_EXPORT const unsigned char TexiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Texi/PublicHeader.h>
+
+
