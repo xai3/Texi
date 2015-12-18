5146196889f5692ed4e26511786177bbc3440f40
yukiasai <yukiasai@gmail.com>
2015-12-18 17:32:33 +0900

    Fix: can't access to Style's initializer.

--- Classes/Style.swift
+++ Classes/Style.swift
@@ -11,4 +11,9 @@ import Foundation
 public struct Style {
     public var color: UIColor
     public var font: UIFont
+    
+    public init(color: UIColor, font: UIFont) {
+        self.color = color
+        self.font = font
+    }
 }
